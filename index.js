03dc23968c5b6b4de7d121d4b3b07bc7606af107
nkrumahinc <nkrumahthis@gmail.com>
2021-06-30 10:16:28 +0000

    hello world

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const port = 3000;
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+// simple route
+app.get("/", (req, res) => {
+    res.json(
+        { message: "Welcome to DextBoard-T" }
+    );
+})
+
+// set port, listen for requests
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+})
