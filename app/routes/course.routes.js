7bf5e0ea1ee4aca7ccb1d3c871c54f3a267c463f
nkrumahinc <nkrumahthis@gmail.com>
2021-07-09 16:46:02 +0000

    feat(courses): courses api endpoints

--- app/routes/course.routes.js
+++ app/routes/course.routes.js
@@ -3,13 +3,13 @@ module.exports = app => {
     const courses = require("../controllers/course.controller.js")
 
     // create a new course
-    app.post("/course", courses.create);
+    app.post("/courses", courses.create);
 
     // retrieve all courses
-    app.get("/course", courses.findAll);
+    app.get("/courses", courses.findAll);
 
     // retrieve a single course with courseId
-    app.get("/course/:id", courses.findOne);
+    app.get("/courses/:id", courses.findOne);
 
     // update a course with courseId
     app.put("/courses/:id", courses.update)
--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-const HomeRouter = require('./app/routes/home.routes.js');
+const HomeRouter = require('./app/routes/home.routes');
 const UserRouter = require('./app/routes/user.routes');
+const CourseRouter = require('./app/routes/course.routes')
 
 HomeRouter(app);
 UserRouter(app);
+CourseRouter(app);
 
 // set port, listen for requests
 app.listen(port, () => {
