8537e9fd2e50a16c8a5eda5e5263d13f187543c7
nkrumahinc <nkrumahthis@gmail.com>
2021-07-09 16:46:02 +0000

    mend

--- app/routes/course.routes.js
+++ app/routes/course.routes.js
@@ -3,10 +3,10 @@ module.exports = app => {
     const courses = require("../controllers/course.controller.js")
 
     // create a new course
-    app.post("/course", courses.create);
+    app.post("/courses", courses.create);
 
     // retrieve all courses
-    app.get("/course", courses.findAll);
+    app.get("/courses", courses.findAll);
 
     // retrieve a single course with courseId
     app.get("/course/:id", courses.findOne);
