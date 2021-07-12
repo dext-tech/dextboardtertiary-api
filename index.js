51111918752515c32da6b5622cbc1038f74d850a
nkrumahinc <nkrumahthis@gmail.com>
2021-07-12 10:27:29 +0000

    add middleware

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
-
-const port = 3000;
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const morgan = require('morgan');
+const lodash = require('lodash');
 
 const app = express();
+
+// set up middleware
 app.use(express.json());
+app.use(fileUpload({createParentPath:true}));
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
 
 const HomeRouter = require('./app/routes/home.routes');
 const UserRouter = require('./app/routes/user.routes');
@@ -14,6 +21,8 @@ UserRouter(app);
 CourseRouter(app);
 
 // set port, listen for requests
+const port = 3000;
+
 app.listen(port, () => {
     console.log(`dbt server is running on localhost:${port}`);
 })
