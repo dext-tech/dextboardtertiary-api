1520db0ef7c04c81e9cb164a6164655e9772f303
nkrumahinc <nkrumahthis@gmail.com>
2021-07-22 11:04:05 +0000

    feat (user) : controller and model

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,130 @@
+const User = require("../models/user.model")
+
+// create and save a new user
+exports.create = (request, response) => {
+    // validate request
+    if(!request.body){
+        response.status(400).send({
+            message: "content cannot be empty!"
+        })
+    }
+
+    // create a user
+    const user = new User({
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        email: request.body.email,
+        password: request.body.password,
+        token: request.body.token
+    })
+
+    // save user in the database
+    User.create(user, (err, data)=>{
+        if(err){
+            response.status(500).send({
+                message: err.message || "some error occurred while creating the user"
+            })
+        } else {
+            response.send(data)
+        }
+    })
+}
+
+// find all users
+exports.findAll = (request, response) => {
+    User.getAll((err, data) => {
+        if(err){
+            response.status(500).send({
+                message: err.message || "some error occurred while retrieving users"
+            })
+        } else {
+            response.send(data);
+        }
+    })
+}
+
+// find a single user with userid
+exports.findOne = (request, response) => {
+    User.findById(request.params.id, (error, data) => {
+        if(error){
+            if(error.kind === "not_found"){
+                response.status(404).send({
+                    message: `Could not find user with id ${request.params.id}`
+                })
+            } else {
+                response.status(500).send({
+                    message: `error finding user with id ${request.params.id}`
+                })
+            }
+        } else {
+            response.send(data)
+        }
+    })
+}
+
+exports.findByEmail = (request, response) => {
+    User.findByEmail(request.params.email, (error, data) => {
+        if(error){
+            if(error.kind === "not_found"){
+                response.status(404).send({
+                    message: `Could not find user with email ${request.params.email}`
+                })
+            } else {
+                response.status(500).send({
+                    message: `error finding user with email ${request.params.email}`
+                })
+            }
+        } else {
+            response.send(data)
+        }
+    })
+}
+
+exports.update = (request, response) => {
+    // validate request
+    if(!request.body){
+        response.status(400).send({
+            message: "content cannot be empty"
+        })
+    }
+
+    User.updateById(
+        request.params.id,
+        new User(request.body),
+        (error, data) => {
+            if(error){
+                if(error.kind === "not_found"){
+                    response.status(404).send({
+                        message: "could not find user with id " + request.params.id
+                    })
+                } else {
+                    response.status(500).send({
+                        message: "error updating user with id " + request.params.id
+                    })
+                }
+            } else {
+                response.send(data)
+            }
+        }
+    )
+
+    // delete a user with the specified userid in the request
+    exports.delete = (request, response) => {
+        User.remove(request.params.id, (error, data) => {
+            if(error){
+                if(error.kind === "not_found"){
+                    response.status(404).send({
+                        message: "could not find user with id " + request.params.id
+                    })
+                } else {
+                    response.status(500).send({
+                        message: "could not delete user with id " + request.params.id
+                    })
+                }
+            } else {
+                response.send({message: "user was delete successfully"})
+            }
+        })
+    }
+}
+
---
+++ app/models/user.model.js
@@ -0,0 +1,124 @@
+const sql = require("./db")
+const moment = require('moment')
+
+// constructor
+const User = function(user){
+    this.id = user.id
+    this.first_name = user.first_name
+    this.last_name = user.last_name
+    this.email = user.email
+    this.password = user.password
+    this.token = user.token
+}
+
+User.create = (user, result) => {
+    sql.query("INSERT INTO users SET ?", user,
+        (err, res) => {
+            if(err){
+                console.log("error: ", err);
+                result(err, null);
+                return;
+            }
+
+            console.log("create user: ", { id: res.insertId, ... user });
+            result(null, { id: res.insertId, ...user });
+        }
+    )
+}   
+
+User.findById = (id, result) => {
+    sql.query("SELECT * FROM users WHERE id = " + id,
+        (err, res) => {
+            if(err){
+                console.log("error: ", err)
+                result(err, null)
+                return
+            }
+            if(res.length){
+                console.log("found user: ", res[0])
+                result(null, res[0])
+                return
+            }
+
+            result({ kind: "not_found" }, null)
+        }
+    )
+}
+
+User.findByEmail = (email, result) => {
+    sql.query("SELECT * FROM users WHERE email = " + email,
+        (err, res) => {
+            if(err){
+                console.log("error: ", err)
+                result(err, null)
+                return
+            }
+            if(res.length){
+                console.log("found user: ", res[0])
+                result(null, res[0])
+                return
+            }
+
+            result({ kind: "not_found" }, null)
+        }
+    )
+}
+
+User.getAll = result => {
+    sql.query("SELECT * FROM users",
+        (err, res) => {
+            if(err){
+                console.log("error: ", err);
+                result(null, err);
+                return
+            }
+            console.log("users: ", res)
+            result(null, res)
+        }
+    )
+}
+
+User.updateById = (id, user, result) => {
+    sql.query("UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE id = ? ",
+        [user.first_name, user.last_name, user.email], // php myadmin
+        (err, res) => {
+            if(err){
+                console.log("error: ", err)
+                result(null, err)
+                return
+            }
+
+            if(res.affectedRows == 0){
+                result({kind: "not_found"}, null)
+                return
+            }
+
+            console.log("updated user: ", {id: id, ...user})
+            result(null, {id:id, ...user})
+        }
+    )
+}
+
+User.remove = (id, result) => {
+    sql.query(
+        "DELETE FROM users WHERE id = ? ",
+        id,
+        (err, res) => {
+            if(err){
+                console.log("error: ", err)
+                result(null, err)
+                return
+            }
+            if(res.affectedRows == 0){
+                // not found user with the id
+                result({kind: "not_found"}, null)
+                return
+            }
+            console.log("deleted user with id: ", id);
+            result(null, res)
+        }
+
+    )
+}
+
+module.exports = User;
